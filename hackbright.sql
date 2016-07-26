3355ba3cf03904de0a86bacb548b53f3e2bc95bb
Hackbright Student <info@hackbrightacademy.com>
2016-07-25 17:54:33 -0700

    Completed further study'

--- hackbright.sql
+++ hackbright.sql
@@ -30,6 +30,41 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
+-- Name: grades; Type: TABLE; Schema: public; Owner: user; Tablespace: 
+--
+
+CREATE TABLE grades (
+    id integer NOT NULL,
+    student_github character varying(30),
+    project_title character varying(30),
+    grade integer
+);
+
+
+ALTER TABLE public.grades OWNER TO "user";
+
+--
+-- Name: grades_id_seq; Type: SEQUENCE; Schema: public; Owner: user
+--
+
+CREATE SEQUENCE grades_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.grades_id_seq OWNER TO "user";
+
+--
+-- Name: grades_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: user
+--
+
+ALTER SEQUENCE grades_id_seq OWNED BY grades.id;
+
+
+--
 -- Name: projects; Type: TABLE; Schema: public; Owner: user; Tablespace: 
 --
 
@@ -71,17 +106,62 @@ ALTER SEQUENCE projects_id_seq OWNED BY projects.id;
 CREATE TABLE students (
     first_name character varying(30),
     last_name character varying(30),
-    github character varying(30)
+    github character varying(30) NOT NULL
 );
 
 
 ALTER TABLE public.students OWNER TO "user";
 
 --
+-- Name: reportcardview; Type: VIEW; Schema: public; Owner: user
+--
+
+CREATE VIEW reportcardview AS
+ SELECT students.first_name,
+    students.last_name,
+    projects.title,
+    projects.max_grade,
+    grades.grade
+   FROM ((students
+     JOIN grades ON (((students.github)::text = (grades.student_github)::text)))
+     JOIN projects ON (((projects.title)::text = (grades.project_title)::text)));
+
+
+ALTER TABLE public.reportcardview OWNER TO "user";
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: user
+--
+
+ALTER TABLE ONLY grades ALTER COLUMN id SET DEFAULT nextval('grades_id_seq'::regclass);
+
+
+--
 -- Name: id; Type: DEFAULT; Schema: public; Owner: user
 --
 
 ALTER TABLE ONLY projects ALTER COLUMN id SET DEFAULT nextval('projects_id_seq'::regclass);
+
+
+--
+-- Data for Name: grades; Type: TABLE DATA; Schema: public; Owner: user
+--
+
+COPY grades (id, student_github, project_title, grade) FROM stdin;
+1	jhacks	Markov	10
+2	jhacks	Blockly	2
+3	sdevelops	Blockly	100
+4	sdevelops	Markov	50
+6	telula67	Blockly	23
+5	batman	Markov	75
+\.
+
+
+--
+-- Name: grades_id_seq; Type: SEQUENCE SET; Schema: public; Owner: user
+--
+
+SELECT pg_catalog.setval('grades_id_seq', 6, true);
 
 
 --
@@ -112,11 +192,27 @@ Sarah	Developer	sdevelops
 
 
 --
+-- Name: grades_pkey; Type: CONSTRAINT; Schema: public; Owner: user; Tablespace: 
+--
+
+ALTER TABLE ONLY grades
+    ADD CONSTRAINT grades_pkey PRIMARY KEY (id);
+
+
+--
 -- Name: projects_pkey; Type: CONSTRAINT; Schema: public; Owner: user; Tablespace: 
 --
 
 ALTER TABLE ONLY projects
     ADD CONSTRAINT projects_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: students_pkey; Type: CONSTRAINT; Schema: public; Owner: user; Tablespace: 
+--
+
+ALTER TABLE ONLY students
+    ADD CONSTRAINT students_pkey PRIMARY KEY (github);
 
 
 --
